83b8faed4e8e72a3793876be766f1fbb6d8d9994
Atomanti007 <atomanti007@citromail.hu>
2021-04-25 23:20:09 +0200

    Bash generator

---
+++ install.sh
@@ -0,0 +1,11 @@
+cd ~ || exit
+
+# Download
+curl -sSL https://raw.githubusercontent.com/Atomanti007/generator/main/generate.sh -o pgen
+chmod +x pgen
+sudo mv pgen /usr/local/bin/
+
+# Download
+curl -sSL https://git.io/get-mo -o mo
+chmod +x mo
+sudo mv mo /usr/local/bin/
---
+++ install_local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -R /mnt/c/Projects/storesync.io/generator $HOME/generator
